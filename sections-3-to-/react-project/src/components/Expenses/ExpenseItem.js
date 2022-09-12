083f97155223da81af38a38e5d602c4b2adea380
hegnemurd <hegne.murd@gmail.com>
2022-09-12 22:55:47 +0100

    Section 5 - Assignment of filtering

--- sections-3-to-/react-project/src/components/Expenses/ExpenseItem.js
+++ sections-3-to-/react-project/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem evaluated by React");
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
-
   return (
     <Card
       className="expense-item"
@@ -20,10 +12,9 @@ const ExpenseItem = (props) => {
     >
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
