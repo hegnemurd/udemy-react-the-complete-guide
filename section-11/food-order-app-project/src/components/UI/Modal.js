8a6ee4b05e3398a86747ed70919d4554f93eb37e
hegnemurd <hegne.murd@gmail.com>
2022-09-22 22:23:52 +0100

    Section 11

---
+++ section-11/food-order-app-project/src/components/UI/Modal.js
@@ -0,0 +1,17 @@
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}></div>
+    </div>
+  );
+};
+
+const Modal = (props) => {};
+
+export default Modal;
