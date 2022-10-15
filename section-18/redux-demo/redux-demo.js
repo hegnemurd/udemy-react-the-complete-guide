f3d713628c72b964c11e99dc57bc29d3fdfb8aaf
hegnemurd <hegne.murd@gmail.com>
2022-10-15 16:23:40 +0100

    Section 18

--- section-18/redux-demo/redux-demo.js
+++ section-18/redux-demo/redux-demo.js
@@ -1,10 +1,21 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return { counter: state.counter + 1 };
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
 };
 
-const store = redux.createStore(counterReducer);
+const store = redux.legacy_createStore(counterReducer);
 
 const counterSubscriber = () => {
   const latestState = store.getState();
@@ -14,3 +25,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
