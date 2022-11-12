1e63b30bf60b088a261db7b2c4106fdfaaa3bcf4
hegnemurd <hegne.murd@gmail.com>
2022-11-12 21:07:55 +0000

    Section 20

---
+++ section-20/react-router-v6.4-demo/src/pages/BlogPosts.jsx
@@ -0,0 +1,17 @@
+import Posts from "../components/Posts";
+import { getPosts } from "../util/api";
+
+function BlogPostsPage() {
+  return (
+    <>
+      <h1>Our Blog Posts</h1>
+      <Posts blogPosts={posts} />
+    </>
+  );
+}
+
+export default BlogPostsPage;
+
+export function loader() {
+  return getPosts();
+}
