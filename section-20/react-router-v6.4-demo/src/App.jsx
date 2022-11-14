f43153abd196216181c76531be29f4501caa763f
hegnemurd <hegne.murd@gmail.com>
2022-11-14 21:06:06 +0000

    Section 20

--- section-20/react-router-v6.4-demo/src/App.jsx
+++ section-20/react-router-v6.4-demo/src/App.jsx
@@ -1,27 +1,50 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 import BlogLayout from "./pages/BlogLayout";
 import BlogPostsPage, { loader as blogPostsLoader } from "./pages/BlogPosts";
-import NewPostPage from "./pages/NewPost";
-import PostDetailPage from "./pages/PostDetail";
-import RootLayout from "./components/RootLayout";
+import ErrorPage from "./pages/Error";
+import NewPostPage, { action as newPostAction } from "./pages/NewPost";
+import PostDetailPage, { loader as blogPostLoader } from "./pages/PostDetail";
+import RootLayout from "./pages/RootLayout";
 import WelcomePage from "./pages/Welcome";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<RootLayout />}
+      errorElement={<ErrorPage />}
+      // errorElement={<p>An error occurred!</p>}
+    >
+      <Route index element={<WelcomePage />} />
+      <Route path="/blog" element={<BlogLayout />}>
+        <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
+        <Route
+          path=":id"
+          element={<PostDetailPage />}
+          loader={blogPostLoader}
+        />
+      </Route>
+      <Route
+        path="/blog/new"
+        element={<NewPostPage />}
+        action={newPostAction}
+      />
+    </Route>
+  )
+);
+
+// const router = createBrowserRouter([
+//   {path: "/", element: <WelcomePage/>}
+// ]);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/blog" element={<BlogLayout />}>
-            <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
-            <Route path=":id" element={<PostDetailPage />} />
-          </Route>
-          <Route path="/blog/new" element={<NewPostPage />} />
-        </Routes>
-      </RootLayout>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
