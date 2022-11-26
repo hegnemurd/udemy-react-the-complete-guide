46d2e56901a95d38c550e6b2d3fdb6cc75267e6f
hegnemurd <hegne.murd@gmail.com>
2022-11-26 21:01:42 +0000

    Section 23

---
+++ section-23/nextjs-demo-1/pages/news/index.js
@@ -0,0 +1,21 @@
+// our-domain.com/news
+import { Link } from "react";
+import { Fragment } from "react";
+
+function NewsPage() {
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-A-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default NewsPage;
