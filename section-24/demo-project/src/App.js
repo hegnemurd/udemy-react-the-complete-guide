ba6a469c4b1392e658bd373b999e4e42750d8ce7
hegnemurd <hegne.murd@gmail.com>
2022-11-30 22:04:33 +0000

    Section 24

--- section-24/demo-project/src/App.js
+++ section-24/demo-project/src/App.js
@@ -8,6 +8,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -22,8 +23,27 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        {this.state.showBlock ? (
+          <div
+            style={{
+              backgroundColor: "red",
+              width: 100,
+              height: 100,
+              margin: "auto",
+            }}
+          ></div>
+        ) : null}
+        {this.state.modalIsOpen ? <Modal closed={this.closeModal} /> : null}
+        {this.state.modalIsOpen ? <Backdrop /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
