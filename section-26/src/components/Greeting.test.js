32742a6e9f212859f6f335e7cfdaaebafcd8406b
hegnemurd <hegne.murd@gmail.com>
2022-12-04 15:23:38 +0000

    Section 26

---
+++ section-26/src/components/Greeting.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Greeting from "./Greeting";
+
+describe("Greeting component", () => {
+  test("renders 'Hello World' as a text", () => {
+    render(<Greeting />);
+
+    const helloWorldElement = screen.getByText("Hello World!");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders 'good to see you' if the button was NOT clicked", () => {
+    render(<Greeting />);
+
+    const goodToSeeElement = screen.getByText("good to see you", {
+      exact: false,
+    });
+    expect(goodToSeeElement).toBeInTheDocument();
+  });
+
+  test("renders 'Changed!' if the button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const changedElement = screen.getByText("Changed!");
+    expect(changedElement).toBeInTheDocument();
+  });
+
+  test("does not render 'good to see you' if the button was clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const changedElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(changedElement).toBeNull();
+  });
+});
