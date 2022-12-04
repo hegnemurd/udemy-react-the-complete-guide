4dfc09a6930e8542c021ee3badc54093740da1fc
hegnemurd <hegne.murd@gmail.com>
2022-12-04 14:07:17 +0000

    Section 25

--- section-25/src/components/Products/ProductItem.js
+++ section-25/src/components/Products/ProductItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import Card from "../UI/Card";
+import { useStore } from "../../hooks-store/store";
 import "./ProductItem.css";
-// import { toggleFav } from '../../store/actions/products';
 
-const ProductItem = (props) => {
+const ProductItem = React.memo((props) => {
+  const dispatch = useStore(false)[1];
+
   const toggleFavHandler = () => {
-    // dispatch(toggleFav(props.id));
+    // toggleFav(props.id);
+    dispatch("TOGGLE_FAV", props.id);
   };
 
   return (
@@ -23,6 +26,6 @@ const ProductItem = (props) => {
       </div>
     </Card>
   );
-};
+});
 
 export default ProductItem;
